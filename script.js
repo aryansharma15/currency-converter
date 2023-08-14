cfa2883a2ccd9a2de691eebaf6cf8fb2aad6912d
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-08-15 00:17:36 +0530

    ael error

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-let select = document.getElementById("from-curr");
+let fromCurr = document.getElementById("from-curr");
+let toCurr = document.getElementById("to-curr");
 
-let curr_in = document.getElementsByClassName("from-curr-inp");
-let curr_out = document.getElementsByClassName("to-curr-inp");
+let firstCur = document.getElementsByClassName("from-curr-inp");
+let secondCur = document.getElementsByClassName("to-curr-inp");
 
-console.log(select);
-console.log(curr_in);
-console.log(curr_out);
+let conv_btn = document.getElementsByClassName("conv-btn");
+let inpNum;
+let outNum;
+
+const api = "https://api.exchangeratesapi.io/v1/latest";
+
+// console.log(select);
+// console.log(curr_in);
+// console.log(curr_out);
+// console.log(conv_btn);
+// console.log(to_curr);
+
+fromCurr.addEventListener("change", (event) => {
+	inpNum = `${event.target.value}`;
+});
+
+toCurr.addEventListener("change", (event) => {
+	outNum = `${event.target.value}`;
+});
+
+firstCur.addEventListener("input", updateValue);
+
+function updateValue(x) {
+	searchValue = x.target.value;
+}
+
+conv_btn.addEventListener("click", getResult);
+
+function getResult() {
+	fetch(`${api}`)
+		.then((currency) => {
+			return currency.json();
+		})
+		.then(displayResult);
+}
+
+function displayResult(currency) {
+	let fromRate = currency.rates[inpNum];
+	let toRate = currency.rates[outNum];
+	secondCur.innerHTML = ((toRate / fromRate) * searchValue).toFixed(2);
+}

589fd9e310ff6122cc7f440f37fe1a6cd7b4b91a
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-08-14 23:31:34 +0530

    translated

---
+++ script.js
@@ -0,0 +1,8 @@
+let select = document.getElementById("from-curr");
+
+let curr_in = document.getElementsByClassName("from-curr-inp");
+let curr_out = document.getElementsByClassName("to-curr-inp");
+
+console.log(select);
+console.log(curr_in);
+console.log(curr_out);

98a6a822a1d7a50c95acf236eeaa57d813ac11c7
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-08-15 20:50:30 +0530

    2 bugs: rate, final output

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let toCurr = document.getElementById("to-curr");
 let firstCurNum = document.getElementsByClassName("from-curr-inp")[0];
 let secondCurNum = document.getElementsByClassName("to-curr-inp")[0];
 
-let swapButton = document.getElementsByClassName("swap");
+let swapButton = document.getElementsByClassName("swap")[0];
 let exchRate = document.getElementsByClassName("rate-box");
 
 let conv_btn = document.getElementsByClassName("conv-btn")[0];
@@ -23,8 +23,8 @@ let searchValue;
 // 	outCur = event.target.value;
 // });
 
-inpCur = firstCurNum.value;
-outCur = secondCurNum.value;
+// inpCur = firstCurNum.value;
+// outCur = secondCurNum.value;
 
 firstCurNum.addEventListener("input", updateValue);
 
@@ -32,24 +32,49 @@ function updateValue(x) {
 	searchValue = x.target.value;
 }
 
-conv_btn.addEventListener("click", getResult);
+// conv_btn.addEventListener("click", getResult);
 
-function getResult() {
-	fetch(`${api}`)
-		.then((currency) => {
-			return currency.json();
-		})
-		.then(displayResult)
-		.catch((error) => {
-			console.error(error);
+// function getResult() {
+// 	fetch(`${api}`)
+// 		.then((currency) => {
+// 			return currency.json();
+// 		})
+// 		.then(displayResult)
+// 		.catch((error) => {
+// 			console.error(error);
+// 		});
+// }
+
+// function displayResult(currency) {
+// 	let fromRate = currency.rates[inpCur];
+// 	let toRate = currency.rates[outCur];
+// 	secondCurNum.innerHTML = ((toRate / fromRate) * searchValue).toFixed(2);
+// 	// } else {
+// 	// 	secondCur.innerHTML = "Currency not supported";
+// 	// }
+// }
+
+function calc() {
+	const inpCur = fromCurr.value;
+	const outCur = toCurr.value;
+
+	fetch(`https://api.exchangerate-api.com/v4/latest/${inpCur}`)
+		.then((res) => res.json())
+		.then((data) => {
+			const rate = data.rates[outCur];
+
+			exchRate.textContent = `1 ${fromCurr} = ${rate.toFixed(4)} ${toCurr}`;
 		});
 }
 
-function displayResult(currency) {
-	let fromRate = currency.rates[inpCur];
-	let toRate = currency.rates[outCur];
-	secondCurNum.innerHTML = ((toRate / fromRate) * searchValue).toFixed(2);
-	// } else {
-	// 	secondCur.innerHTML = "Currency not supported";
-	// }
+function listeners() {
+	swapButton.addEventListener("click", () => {
+		[fromCurr.value, toCurr.value] = [toCurr.value, fromCurr.value];
+	});
+	calc();
 }
+
+window.onload = () => {
+	listeners();
+	calc();
+};

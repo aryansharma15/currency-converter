9a19fbf90fa79b2a4e8ba6f26ea9d0c32bc30c62
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-08-15 19:32:33 +0530

    swap functionality

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 const api = "http://openexchangerates.org/api/latest.json?app_id=477612ac55004129901ccc7f00e61446";
-// const access_key = "0347097f865f328f5107214788fd06cc";
 
 console.log(api);
 let fromCurr = document.getElementById("from-curr");
 let toCurr = document.getElementById("to-curr");
 
-let firstCur = document.getElementsByClassName("from-curr-inp")[0];
-let secondCur = document.getElementsByClassName("to-curr-inp")[0];
+let firstCurNum = document.getElementsByClassName("from-curr-inp")[0];
+let secondCurNum = document.getElementsByClassName("to-curr-inp")[0];
+
+let swapButton = document.getElementsByClassName("swap");
+let exchRate = document.getElementsByClassName("rate-box");
 
 let conv_btn = document.getElementsByClassName("conv-btn")[0];
-let inpNum;
-let outNum;
+let inpCur;
+let outCur;
 let searchValue;
 
-fromCurr.addEventListener("change", (event) => {
-	inpNum = event.target.value;
-});
+// fromCurr.addEventListener("change", (event) => {
+// 	inpCur = event.target.value;
+// });
 
-toCurr.addEventListener("change", (event) => {
-	outNum = event.target.value;
-});
+// toCurr.addEventListener("change", (event) => {
+// 	outCur = event.target.value;
+// });
 
-firstCur.addEventListener("input", updateValue);
+inpCur = firstCurNum.value;
+outCur = secondCurNum.value;
+
+firstCurNum.addEventListener("input", updateValue);
 
 function updateValue(x) {
 	searchValue = x.target.value;
@@ -31,16 +36,9 @@ conv_btn.addEventListener("click", getResult);
 
 function getResult() {
 	fetch(`${api}`)
-		.then((response) => {
-			if (response.status === 200) {
-				return response.json();
-			} else {
-				throw new Error(`Something went wrong. Code: ${response.status}`);
-			}
+		.then((currency) => {
+			return currency.json();
 		})
-		// .then((currency) => {
-		// 	return currency.json();
-		// })
 		.then(displayResult)
 		.catch((error) => {
 			console.error(error);
@@ -48,13 +46,10 @@ function getResult() {
 }
 
 function displayResult(currency) {
-	inpNum = inpNum || "";
-	outNum = outNum || "";
-	if (inpNum in currency.rates && outNum in currency.rates) {
-		let fromRate = currency.rates[inpNum];
-		let toRate = currency.rates[outNum];
-		secondCur.innerHTML = ((toRate / fromRate) * searchValue).toFixed(2);
-	} else {
-		secondCur.innerHTML = "Currency not supported";
-	}
+	let fromRate = currency.rates[inpCur];
+	let toRate = currency.rates[outCur];
+	secondCurNum.innerHTML = ((toRate / fromRate) * searchValue).toFixed(2);
+	// } else {
+	// 	secondCur.innerHTML = "Currency not supported";
+	// }
 }
